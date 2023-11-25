ab2de253d16403fc9be4371092a8a2e6c453a34d
dchueri <diegochueri@gmail.com>
2023-11-25 08:31:08 -0300

    add float action button

--- app/src/main/java/com/example/kotlinstudy/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/kotlinstudy/ui/activity/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.kotlinstudy.ui.activity
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlinstudy.R
 import com.example.kotlinstudy.model.Product
 import com.example.kotlinstudy.ui.recyclerview.adapter.ProductsListAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
@@ -19,5 +21,10 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                 Product("Product2", "Description2", BigDecimal("19.99"))
             )
         )
+        val addButton = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        addButton.setOnClickListener {
+            val intent = Intent(this, ProductFormActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/kotlinstudy/ui/activity/ProductFormActivity.kt
+++ app/src/main/java/com/example/kotlinstudy/ui/activity/ProductFormActivity.kt
@@ -1,11 +1,11 @@
-package com.example.kotlinstudy
+package com.example.kotlinstudy.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import com.example.kotlinstudy.R
 import com.example.kotlinstudy.model.Product
 import java.math.BigDecimal
 
@@ -16,8 +16,8 @@ class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
         saveButton.setOnClickListener {
             val titleField = findViewById<EditText>(R.id.title)
             val title = titleField.text.toString()
-            val descritionField = findViewById<EditText>(R.id.description)
-            val description = descritionField.text.toString()
+            val descriptionField = findViewById<EditText>(R.id.description)
+            val description = descriptionField.text.toString()
             val priceField = findViewById<EditText>(R.id.price)
             val price = priceField.text.toString()
             val handledPrice = if (price.isBlank()) {
